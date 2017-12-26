08c98c2af15f7caeae447849e9dd27f14f32610f
seanhome <homeofsean@gmail.com>
2017-12-26 11:04:37 +0000

    inital file up date

---
+++ Sean _1st_program.py
@@ -0,0 +1,11 @@
+# testing vs cde
+"""
+This is sean testing the vs code to get it to generate a grap out put
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2 * np.pi, 1000)
+y = np.sin(x)
+plt.plot(np.cos(x), 2* y)
+plt.show()
