ff273184338bc6b07605d8620be4362242df40f6
seanhome <homeofsean@gmail.com>
2017-12-26 11:21:05 +0000

    improved theh variable names

--- Sean _1st_program.py
+++ Sean _1st_program.py
@@ -1,11 +1,13 @@
 # testing vs cde
 """
-This is sean testing the vs code to get it to generate a grap out put
+This is sean testing the vs code to get it to generate a graph out put
+
+I made chanegs
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 2 * np.pi, 1000)
-y = np.sin(x)
-plt.plot(np.cos(x), 2* y)
+XAXIS = np.linspace(0, 2 * np.pi, 1000)
+YAXIS = np.sin(XAXIS)
+plt.plot(XAXIS, YAXIS)
 plt.show()
